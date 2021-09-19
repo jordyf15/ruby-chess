be466b066966fa3c6ec7bc6e1055f59b5ddb732f
jordyf15 <jordyferdian88@gmail.com>
2021-09-19 18:47:32 +0700

    edited initialize method of Board class for pieces starting coordination

--- board.rb
+++ board.rb
@@ -2,12 +2,24 @@ require 'colorize'
 
 class Board
   attr_reader :board
-  def initialize
+  def initialize white_player_pieces, black_player_pieces
     @board = (1..8).to_a.reverse.map do |row|
       [{coor: "A#{row}", piece: nil}, {coor: "B#{row}", piece: nil}, 
         {coor: "C#{row}", piece: nil}, {coor: "D#{row}", piece: nil},
         {coor: "E#{row}", piece: nil}, {coor: "F#{row}", piece: nil},
         {coor: "G#{row}", piece: nil}, {coor: "H#{row}", piece: nil},]
+    end
+
+    white_player_pieces.each do |piece|
+      x = piece[:coor][:x]
+      y = piece[:coor][:y]
+      @board[x][y] = piece
+    end
+
+    black_player_pieces.each do |piece|
+      x = piece[:coor][:x]
+      y = piece[:coor][:y]
+      @board[x][y] = piece
     end
   end
 
