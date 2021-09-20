6de745b584604b197ffed1054fb82982882a216c
jordyf15 <jordyferdian88@gmail.com>
2021-09-20 13:49:48 +0700

    created possible_moves method in Knight class

--- piece/knight.rb
+++ piece/knight.rb
@@ -3,6 +3,18 @@ require_relative 'piece'
 class Knight < Piece
   def initialize coor, symbol, owner
     super coor, symbol, owner
-    @possible_moves = [[-1, -2], [-2, -1], [-2, 1], [-1,2], [1,-2], [2,-1], [1, 2], [2, 1]]
+  end
+
+  def possible_moves board 
+    movements = [[-1, -2], [-2, -1], [-2, 1], [-1,2], [1,-2], [2,-1], [1, 2], [2, 1]]
+    possible_moves = movements.map {|movement| {x: @coor[:x]+movement[0] ,y: @coor[:y]+movement[1]}}
+    possible_moves = possible_moves.select do |possible_move|
+      possible_move[:x] <=7 && possible_move[:x]>=0 && possible_move[:y] <=7 && possible_move[:y] >=0
+    end
+    possible_moves = possible_moves.select do |possible_move|
+      board[possible_move[:x]][possible_move[:y]][:piece] == nil || board[possible_move[:x]][possible_move[:y]][:piece].owner != owner
+    end
+
+    possible_moves
   end
 end
