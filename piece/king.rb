b04f7caf1b91bf189e2a3600e45e07ac638bd0c9
jordyf15 <jordyferdian88@gmail.com>
2021-09-20 13:54:25 +0700

    created possible_moves method for King class

--- piece/king.rb
+++ piece/king.rb
@@ -1,5 +1,20 @@
 require_relative 'piece'
 
 class King < Piece
+  def initialize coor, symbol, owner
+    super coor, symbol, owner
+  end
 
+  def possible_moves board
+    movements = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+    possible_moves = movements.map {|movement| {x: @coor[:x]+movement[0] ,y: @coor[:y]+movement[1]}}
+    possible_moves = possible_moves.select do |possible_move|
+      possible_move[:x] <=7 && possible_move[:x]>=0 && possible_move[:y] <=7 && possible_move[:y] >=0
+    end
+    possible_moves = possible_moves.select do |possible_move|
+      board[possible_move[:x]][possible_move[:y]][:piece] == nil || board[possible_move[:x]][possible_move[:y]][:piece].owner != owner
+    end
+
+    possible_moves
+  end
 end
