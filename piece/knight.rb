4c72ad22afa6a92c94f9fa0077750aeb9c952f8e
jordyf15 <jordyferdian88@gmail.com>
2021-09-20 09:41:50 +0700

    add possible_moves instance variable for knight class

--- piece/knight.rb
+++ piece/knight.rb
@@ -1,5 +1,8 @@
 require_relative 'piece'
 
 class Knight < Piece
-
+  def initialize coor, symbol, owner
+    super coor, symbol, owner
+    @possible_moves = [[-1, -2], [-2, -1], [-2, 1], [-1,2], [1,-2], [2,-1], [1, 2], [2, 1]]
+  end
 end
