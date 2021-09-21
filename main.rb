fa44fbfcad5bd0b17b25916d27779bcaa3e9ad92
jordyf15 <jordyferdian88@gmail.com>
2021-09-21 12:03:49 +0700

    edited main for testing'

--- main.rb
+++ main.rb
@@ -1,8 +1,7 @@
 require_relative "board"
-require "./player/white_player"
-require "./player/black_player"
+require_relative "game"
+require_relative "./player/white_player"
+require_relative "./player/black_player"
 
-white_player = WhitePlayer.new "white"
-black_player = BlackPlayer.new "black"
-board = Board.new white_player.pieces, black_player.pieces
-board.print_board
+game = Game.new
+game.play
