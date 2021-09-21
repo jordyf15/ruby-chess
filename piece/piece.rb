aacf33b3b9119743db470c9b00d0ddc052003568
jordyf15 <jordyferdian88@gmail.com>
2021-09-21 11:51:09 +0700

    created die method and attr_reader for @alive instance variable in Piece class

--- piece/piece.rb
+++ piece/piece.rb
@@ -1,9 +1,14 @@
 class Piece
-  attr_reader :symbol, :owner
+  attr_reader :symbol, :owner, :alive
   attr_accessor :coor
   def initialize coor, symbol, owner
     @coor = coor
     @symbol = symbol
     @owner = owner
+    @alive = true
+  end
+
+  def die
+    @alive = false
   end
 end
