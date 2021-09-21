16775b06006e474fcb74b6124a35a62f2f9d0a18
jordyf15 <jordyferdian88@gmail.com>
2021-09-21 07:32:17 +0700

    created coor_display_formatter method for ChessUtil module

--- util/chess_util.rb
+++ util/chess_util.rb
@@ -40,6 +40,46 @@ module ChessUtil
     {x: x, y: y}
   end
 
+  def coor_display_formatter coor 
+    x = case coor[:x]
+    when 0
+      '8'
+    when 1
+      '7'
+    when 2
+      '6'
+    when 3
+      '5'
+    when 4
+      '4'
+    when 5
+      '3'
+    when 6
+      '2'
+    else
+      '1'
+    end
+    y = case coor[:y]
+    when 0
+      'A'
+    when 1
+      'B'
+    when 2
+      'C'
+    when 3
+      'D'
+    when 4
+      'E'
+    when 5
+      'F'
+    when 6
+      'G'
+    else
+      'H'
+    end
+    "#{y}#{x}"
+  end
+
   def valid_coor? coor
     splitted_coor = coor.split('')
     return false if coor.size != 2
