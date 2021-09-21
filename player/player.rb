9b80361ffe0887e8d00d31e4eba6cfbf487ed375
jordyf15 <jordyferdian88@gmail.com>
2021-09-21 11:17:02 +0700

    fixed bug in choose_piece where player able to choose an unmoveable piece

--- player/player.rb
+++ player/player.rb
@@ -2,14 +2,14 @@ require_relative '../util/chess_util'
 
 class Player
   include ChessUtil
-  attr_reader :pieces
+  attr_reader :pieces, :name
   def initialize name, pieces
     @name = name
     @pieces = pieces
   end
 
   def player_move board
-    piece = choose_piece
+    piece = choose_piece board
     possible_moves = piece.possible_moves(board)
     available_move_options = possible_moves.map {|possible_move| coor_display_formatter possible_move}
     piece_movement =  move_piece available_move_options
@@ -29,18 +29,30 @@ class Player
     formatted_coor = coor_converter(coor)
   end
 
-  def choose_piece
+  def choose_piece board
     print "Please choose the piece you want to move: "
     coor = gets.chomp
-    until valid_coor?(coor) && valid_piece?(coor_converter(coor))
+    until valid_coor?(coor) && valid_piece?(coor) && moveable_piece?(coor, board)
+      puts "That piece has no legal moves for now." unless moveable_piece?(coor, board)
       print "Please input a valid coordination of one of your piece: "
       coor = gets.chomp
     end
+    get_player_piece coor
+  end
+
+  def valid_piece? coor
+    formatted_coor = coor_converter(coor)
+    @pieces.any? {|piece| piece.coor == formatted_coor}
+  end
+
+  def get_player_piece coor
     formatted_coor = coor_converter(coor)
     @pieces.select {|piece| piece.coor == formatted_coor}[0]
   end
 
-  def valid_piece? coor
-    @pieces.any? {|piece| piece.coor == coor}
+  def moveable_piece? coor, board
+    piece = get_player_piece coor
+    return true if piece.possible_moves(board).size > 0
+    false
   end
 end
