495cda6eb29873132dd7c0d080de582d6db07f21
jordyf15 <jordyferdian88@gmail.com>
2021-09-21 08:01:31 +0700

    created move_piece method for Player class

--- player/player.rb
+++ player/player.rb
@@ -14,6 +14,17 @@ class Player
     p possible_moves.map {|possible_move| coor_display_formatter possible_move} 
   end
 
+  def move_piece available_move_options
+    p available_move_options
+    print "Please input the coordination you want the piece to move: "
+    coor = gets.chomp
+    until available_move_options.include? coor
+      print "Please input one of the available coordination displayed: "
+      coor = gets.chomp
+    end
+    formatted_coor = coor_converter(coor)
+  end
+
   def choose_piece
     print "Please choose the piece you want to move: "
     coor = gets.chomp
